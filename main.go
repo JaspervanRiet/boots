19fe94b880111fc38a467f862f3728e00ff0a032
Jasper van Riet <jaspervanriet@gmail.com>
2023-05-12 22:55:46 +0200

    Setup HTTP client with auth

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/google/go-github/v52/github"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
+func setupHttpClient() *http.Client {
+	token := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})
+	client := oauth2.NewClient(context.Background(), token)
+	return client
+}
+
 func main() {
-	client := github.NewClient(nil)
-	repos, _, _ := client.Repositories.List(context.Background(), "JaspervanRiet", nil)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error in loading .env file!")
+	}
+
+	httpClient := setupHttpClient()
+	ghClient := github.NewClient(httpClient)
+
+	repos, _, _ := ghClient.Repositories.List(context.Background(), "JaspervanRiet", nil)
+
 	fmt.Println(repos)
 }
